7f32eda2ac18154ae5f154ba8d9ab98b5abab388
laetitiabeles <laetitiabeles9@gmail.com>
2023-12-20 15:43:04 +0100

    change conditions of the full path

--- handle_input.c
+++ handle_input.c
@@ -13,19 +13,31 @@ int handle_input(char *line, char **array, char *full_path)
 	int index;
 
 	index = tokenize_input(line, array);
+	if (index < 0)
+	{
+		perror("Failed to tokenize input");
+		return (-1);
+	}
 
 	if (array[0][0] == '/' || array[0][0] == '.')
 		full_path = strdup(array[0]);
 	else
 		full_path = getpath(array[0]);
 
-	if (full_path != NULL)
+	if (full_path == NULL)
 	{
+		fprintf(stderr, "Command not found: %s\n", array[0]);
+		return (index);
+	}
+
 		if (execute_command(full_path, array) != 0)
 		{
-			cleanup(line, array, full_path);
-			exit(1);
+			perror("can't execute");
+			/* free(line); */
+			free(array);
+			free(full_path);
 		}
-	}
+
 	return (index);
-}
+	}
+
